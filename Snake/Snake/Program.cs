2de51dff7cf48d5851dbfed9f9ecb5d98d33a38b
GruzdovAN <antologyz@gmail.com>
2016-02-18 03:45:50 +0300

    add HorizontalLine and VerticalLine classes

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,20 +11,20 @@ namespace Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1,3,'*');
-            p1.Draw();
+            p1.Drow();
 
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            p2.Drow();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            HorizontalLine lineH = new HorizontalLine(5,10,8,'+');
+            lineH.Drow();
+            VerticalLine lineV = new VerticalLine(9, 20, 10, '+');
+            lineV.Drow();
 
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
+            HorizontalLine lineH2 = new HorizontalLine(5, 10, 20, '+');
+            lineH2.Drow();
+            VerticalLine lineV2 = new VerticalLine(9, 20, 5, '+');
+            lineV2.Drow();
 
             Console.ReadLine();
         }
